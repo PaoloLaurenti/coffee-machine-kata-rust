d00aaa33779f3e531f6eb68264ae96855319c35b
Paolo Laurenti <paolo.laurenti@prima.it>
2023-07-19 15:28:50 +0200

    enable the coffee machine to dispense beverages with a stick when some sugar has been required

--- src/machine.rs
+++ src/machine.rs
@@ -48,13 +48,13 @@ fn build_drink_maker_command(beverage_request: BeverageRequest) -> String {
         BeverageType::HotChocolate => "H",
     };
 
-    let sugar_amount_cmd_part = match beverage_request.sugar_amount {
-        SugarAmount::Zero => "",
-        SugarAmount::One => "1",
-        SugarAmount::Two => "2",
+    let (sugar_amount_cmd_part, stick_cmd_part) = match beverage_request.sugar_amount {
+        SugarAmount::Zero => ("", ""),
+        SugarAmount::One => ("1", "0"),
+        SugarAmount::Two => ("2", "0"),
     };
 
-    format!("{beverage_cmd_part}:{sugar_amount_cmd_part}:")
+    format!("{beverage_cmd_part}:{sugar_amount_cmd_part}:{stick_cmd_part}")
 }
 
 #[cfg(test)]
@@ -88,7 +88,7 @@ mod machine_tests {
     #[test_case(BeverageType::Coffe, "C::" ; "cofee")]
     #[test_case(BeverageType::Tea, "T::" ; "tea")]
     #[test_case(BeverageType::HotChocolate, "H::" ; "hot chocolate")]
-    fn machine_dispenses_beverage_with_no_sugar(
+    fn machine_dispenses_beverage_with_no_sugar_no_stick(
         beverage_type: BeverageType,
         expected_drink_maker_cmd: &str,
     ) {
@@ -119,7 +119,25 @@ mod machine_tests {
 
         let drink_maker_cmds = drink_maker_spy.get_received_commands();
         assert_eq!(1, drink_maker_cmds.len());
-        let sugar_amount_part = drink_maker_cmds[0].split(':').nth(1).unwrap();
-        assert_eq!(sugar_amount_part, expected_sugar_amount_cmd_part)
+        let sugar_amount_cmd_part = drink_maker_cmds[0].split(':').nth(1).unwrap();
+        assert_eq!(sugar_amount_cmd_part, expected_sugar_amount_cmd_part)
+    }
+
+    #[test_case(SugarAmount::One, "0" ; "stick with one sugar")]
+    #[test_case(SugarAmount::Two, "0" ; "stick with  two sugars")]
+    fn machine_dispenses_bverage_with_stick_when_some_sugar_is_requested(
+        sugar_amount: SugarAmount,
+        expected_stick_cmd_part: &str,
+    ) {
+        let drink_maker_spy = DrinkMakerSpy::new();
+        let machine = Machine::new(&drink_maker_spy);
+
+        let beverage_request = BeverageRequest::new(BeverageType::Coffe, sugar_amount);
+        machine.dispense(beverage_request);
+
+        let drink_maker_cmds = drink_maker_spy.get_received_commands();
+        assert_eq!(1, drink_maker_cmds.len());
+        let stick_cmd_part = drink_maker_cmds[0].split(':').nth(2).unwrap();
+        assert_eq!(stick_cmd_part, expected_stick_cmd_part)
     }
 }
